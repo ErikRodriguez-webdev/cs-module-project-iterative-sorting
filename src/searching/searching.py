2a59c8d4e7abb18021c2fabf479167ec7762da62
Erik Rodriguez <eric19.21.03@gmail.com>
2020-07-20 16:32:28 -0700

    Adds code to binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -11,30 +11,32 @@ def linear_search(arr, target):
 
 
 def binary_search(arr, target):
-    pass
+    if len(arr) == 0:
+        return -1
     # Lowest is always 0
     # Highest is always len(list) - 1
     # Middle is Lowest + Highest / 2
 
     # trigger = True
-    # lowest = 0
-    # highest = len(arr) - 1
+    lowest = 0
+    highest = len(arr) - 1
 
     # While loop true:
-    # if target > middle, then set lowest to middle + 1
-    # if target < middle, then set highest to middle - 1
-    # while trigger:
-    #     middle = (lowest + highest) // 2
-    #     print(middle)
+    while True:
+        # if target > middle, then set lowest to middle + 1
+        # if target < middle, then set highest to middle - 1
+        # while trigger:
+        #     middle = (lowest + highest) // 2
+        #     print(middle)
+        middle = (lowest + highest) // 2
 
-    #     if target == middle:
-    #         trigger = False
-    #         return middle
+        if target == arr[middle]:
+            return middle
 
-    #     if target > middle:
-    #         lowest = middle + 1
+        if target > arr[middle]:
+            lowest = middle + 1
 
-    #     if target < middle:
-    #         highest = middle - 1
+        if target < arr[middle]:
+            highest = middle - 1
 
     return -1  # not found
